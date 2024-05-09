b1d75412277975da3b5cf81e7662e1f44204e9f5
Bryton Lacquement <hello@brytonlacquement.com>
2024-05-09 16:43:39 +0200

    HeaderBar: modify the style

--- src/components/organisms/HeaderBar.jsx
+++ src/components/organisms/HeaderBar.jsx
@@ -15,7 +15,7 @@ export default function HeaderBar() {
 
   return (
     <div className="px-5">
-      <ul className="-mb-px flex flex-wrap justify-center gap-2 text-center text-sm font-medium text-neutral-500">
+      <ul className="-mb-px flex flex-wrap justify-center gap-2 text-center text-sm">
         {links.map((link) => {
           // TODO:
           let isActive = link.label == "Table View";
@@ -25,17 +25,16 @@ export default function HeaderBar() {
               <Link
                 to={link.url}
                 className={classNames(
-                  "group inline-flex items-center justify-center rounded-t-lg border-b-2 border-transparent p-4 hover:border-neutral-300 hover:text-neutral-600",
-                  isActive && "!border-teal-600 !text-teal-600",
+                  "group flex border-b-2 border-transparent p-3",
+                  isActive && "!border-orange-400 font-semibold",
                 )}
               >
-                <link.Icon
-                  className={classNames(
-                    "me-2 h-4 w-4 text-neutral-400 group-hover:text-neutral-500",
-                    isActive && "!text-teal-600",
-                  )}
-                />
-                {link.label}
+                <span className="inline-flex items-center justify-center rounded p-1 group-hover:bg-neutral-50">
+                  <link.Icon
+                    className={classNames("me-2 h-4 w-4 text-neutral-500")}
+                  />
+                  {link.label}
+                </span>
               </Link>
             </li>
           );
