15838c22a62cee182a4b07fb55041d22446d3c56
Bryton Lacquement <hello@brytonlacquement.com>
2024-05-10 17:17:32 +0200

    Button: improve the style by state

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -11,12 +11,11 @@ export default function Button({
     <button
       type="button"
       className={classNames(
-        "inline-flex items-center gap-1 rounded-md p-1.5 text-center text-xs font-medium",
+        "inline-flex items-center gap-1 rounded-md p-1.5 text-center text-xs font-medium active:brightness-95",
         variant == "no-outline" && "text-neutral-700 hover:bg-neutral-100",
         variant != "no-outline" && "border",
 
-        variant == "default" &&
-          "ring-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-600",
+        variant == "default" && "bg-white hover:bg-neutral-50",
         variant == "green" &&
           "border-green-700 bg-green-600 text-white hover:bg-green-700",
 
