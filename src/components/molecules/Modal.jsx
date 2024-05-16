29aabf6dc6df132b42e5965ce6f11a77f80c6fe9
Bryton Lacquement <hello@brytonlacquement.com>
2024-05-17 01:51:34 +0200

    Modal: add a fade in/out animation

--- src/components/molecules/Modal.jsx
+++ src/components/molecules/Modal.jsx
@@ -4,13 +4,17 @@ export default function Modal({ show, children, onClose }) {
   return (
     <div
       className={classNames(
-        !show && "hidden",
-        "fixed left-0 right-0 top-0 z-40 flex h-full max-h-full w-full items-center justify-center overflow-y-auto overflow-x-hidden bg-black/50 md:inset-0",
+        !show && "opacity-0 pointer-events-none",
+        "fixed left-0 right-0 top-0 z-40 flex h-full max-h-full w-full items-center justify-center overflow-y-auto overflow-x-hidden bg-black/50 md:inset-0 backdrop-blur-sm transition-all duration-300",
       )}
       onClick={onClose}
     >
       <div
-        className="relative max-h-full w-full max-w-7xl"
+        className=
+        {classNames(
+        "relative max-h-full w-full max-w-7xl transition-all duration-300",
+        !show && "translate-y-10",
+        )}
         onClick={(e) => e.stopPropagation()}
       >
         {children}
