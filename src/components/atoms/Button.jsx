64787b5b95645cd1ad571e40249ab054886b3cf8
Bryton Lacquement <hello@brytonlacquement.com>
2024-06-08 12:50:27 +0200

    fixup! TaskDetails/ActionsCard: add a timer indicator
    This fixes commit a0f7d139aed6da959def87057f8ca329da395cd5.

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -30,7 +30,7 @@ export default function Button({
         variant == "gray" &&
           "border-neutral-700 bg-neutral-600 text-white enabled:hover:bg-neutral-700",
         variant == "gray-outline" &&
-          "border-neutral-500 text-neutral-600 ring-inset ring-neutral-500 enabled:hover:bg-neutral-600 enabled:hover:text-white [&:not(:hover)]:ring-1",
+          "border-neutral-500 text-neutral-700 ring-inset ring-neutral-500 enabled:hover:bg-neutral-600 enabled:hover:text-white [&:not(:hover)]:ring-1",
         variant == "blue" &&
           "border-blue-700 bg-blue-600 text-white enabled:hover:bg-blue-700",
 
