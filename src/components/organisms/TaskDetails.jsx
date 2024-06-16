d553ef5d0ae539b078f6f6027e1a9b36e8e1394d
Bryton Lacquement <hello@brytonlacquement.com>
2024-06-16 23:44:00 +0200

    TaskDetails/Heading2: make it pre

--- src/components/organisms/TaskDetails.jsx
+++ src/components/organisms/TaskDetails.jsx
@@ -265,7 +265,7 @@ function TaskDescription({ task, onSubmit }) {
   return (
     <div className="mb-6 flex items-start justify-between gap-3">
       <Heading2
-        className="!mb-0"
+        className="!mb-0 whitespace-pre-wrap break-all"
         title={task.description}
         subtitle={task.id != 0 && `#${task.id}`}
       />
