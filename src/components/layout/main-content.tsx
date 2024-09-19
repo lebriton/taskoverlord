4b30c3a11ea7e63b4c04de6c734e04907ec3020f
Bryton Lacquement <hello@brytonlacquement.com>
2024-09-19 23:26:01 +0200

    MainContent: create the WIP ConfigurationSection

--- src/components/layout/main-content.tsx
+++ src/components/layout/main-content.tsx
@@ -1,3 +1,41 @@
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { PlusCircleIcon, SlidersHorizontalIcon } from "lucide-react";
+
+interface FilterButtonProps {
+  label: string;
+}
+
+function FilterButton({ label }: FilterButtonProps) {
+  return (
+    <Button variant="outline" className="border-dashed">
+      <PlusCircleIcon className="mr-2 h-4 w-4" /> {label}
+    </Button>
+  );
+}
+
+function ConfigurationSection() {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex flex-1 items-center gap-x-2">
+        <Input placeholder="Filter tasks…" className="w-[150px] lg:w-[250px]" />
+
+        <FilterButton label="Status" />
+        <FilterButton label="Priority" />
+      </div>
+
+      <Button variant="outline">
+        <SlidersHorizontalIcon className="mr-2 size-4" /> View
+      </Button>
+    </div>
+  );
+}
+
 export default function MainContent() {
-  return "WIP - Main content";
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      <ConfigurationSection />
+      WIP - Main content
+    </div>
+  );
 }
