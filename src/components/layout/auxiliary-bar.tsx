c1d60c61188d840f9e6a7be3bac42fe0194f666f
Bryton Lacquement <hello@brytonlacquement.com>
2024-10-06 11:48:58 +0200

    AuxiliaryBar: re-arrange the top buttons

--- src/components/layout/auxiliary-bar.tsx
+++ src/components/layout/auxiliary-bar.tsx
@@ -1,35 +1,43 @@
-import { Separator } from "../ui/separator";
+import FlexLine from "../custom/flex-line";
 import { ButtonList } from "../utils/button-utils";
 import { EmptyState } from "@/components/custom/empty-state";
 import { Button } from "@/components/ui/button";
 import { TooltipWrapper } from "@/components/utils/tooltip-utils";
-import { ChevronDownIcon, ChevronsRightIcon, ChevronUpIcon } from "lucide-react";
+import { ChevronDownIcon, ChevronUpIcon, Ellipsis, XIcon } from "lucide-react";
 
 export default function AuxiliaryBar() {
   return (
     <div className="flex size-full flex-col gap-1.5 bg-muted/40 px-3 py-1.5 text-muted-foreground">
-      <ButtonList>
-        <TooltipWrapper content={<p>Close</p>}>
-          <Button variant="ghost" size="icon">
-            <ChevronsRightIcon />
-          </Button>
-        </TooltipWrapper>
-
-        <Separator className="h-5" orientation="vertical" />
-
-        <ButtonList size="sm">
-          <TooltipWrapper content={<p>Previous task</p>}>
-            <Button variant="ghost" size="icon">
-              <ChevronUpIcon />
-            </Button>
-          </TooltipWrapper>
-          <TooltipWrapper content={<p>Next task</p>}>
-            <Button variant="ghost" size="icon">
-              <ChevronDownIcon />
-            </Button>
-          </TooltipWrapper>
-        </ButtonList>
-      </ButtonList>
+      <FlexLine
+        start={
+          <ButtonList size="sm">
+            <TooltipWrapper content={<p>Previous task</p>}>
+              <Button variant="ghost" size="icon">
+                <ChevronUpIcon />
+              </Button>
+            </TooltipWrapper>
+            <TooltipWrapper content={<p>Next task</p>}>
+              <Button variant="ghost" size="icon">
+                <ChevronDownIcon />
+              </Button>
+            </TooltipWrapper>
+          </ButtonList>
+        }
+        end={
+          <ButtonList size="sm">
+            <TooltipWrapper content={<p>Close</p>}>
+              <Button variant="ghost" size="icon">
+                <Ellipsis />
+              </Button>
+            </TooltipWrapper>
+            <TooltipWrapper content={<p>Close</p>}>
+              <Button variant="ghost" size="icon">
+                <XIcon />
+              </Button>
+            </TooltipWrapper>
+          </ButtonList>
+        }
+      />
 
       <EmptyState title="No items selected" subtitle="Pick a task to display and modify its data." />
     </div>
