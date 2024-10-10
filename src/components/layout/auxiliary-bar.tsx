f0d1e48570775ab36479b00acab6731068789443
Bryton Lacquement <hello@brytonlacquement.com>
2024-10-10 23:03:29 +0200

    qa: move EdiTaskForm to its own file

--- src/components/layout/auxiliary-bar.tsx
+++ src/components/layout/auxiliary-bar.tsx
@@ -1,84 +1,13 @@
 import FlexLine from "../custom/flex-line";
 import { SkeletonForm } from "../custom/skeleton/form-skeleton";
-import { ScrollArea } from "../ui/scroll-area";
-import { ActionButtons, ButtonList } from "../utils/button-utils";
+import { EditTaskForm } from "../features/forms/edit-task-form";
+import { ActionButtons } from "../utils/button-utils";
 import { EmptyState } from "@/components/custom/empty-state";
-import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { useGlobalState } from "@/contexts/global-state";
 import { getTask } from "@/lib/ipc";
-import { Task, taskSchema } from "@/lib/types/task";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { Task } from "@/lib/types/task";
 import { useQuery } from "@tanstack/react-query";
-import { ChevronDownIcon, ChevronUpIcon, EllipsisIcon, SaveIcon, TrashIcon, XIcon } from "lucide-react";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-
-const formSchema = z.object({
-  description: taskSchema.shape.description,
-  status: taskSchema.shape.status,
-});
-
-interface MyFormProps {
-  task: Task;
-}
-
-function MyForm({ task }: MyFormProps) {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: { description: task.description, status: task.status },
-  });
-
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
-  }
-
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex-container flex flex-1 flex-col gap-3">
-        <ScrollArea>
-          <div className="flex-container flex-col space-y-6 px-3">
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Description</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Enter a description…" autoFocus {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="status"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Status</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Enter a description…" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-
-          <div className="h-24" />
-        </ScrollArea>
-
-        <ButtonList className="mx-3 mb-3" orientation="vertical">
-          <Button type="submit" disabled={!form.formState.isDirty}>
-            Save
-          </Button>
-        </ButtonList>
-      </form>
-    </Form>
-  );
-}
+import { ChevronDownIcon, ChevronUpIcon, EllipsisIcon, XIcon } from "lucide-react";
 
 export default function AuxiliaryBar() {
   const { selectedTaskUuid } = useGlobalState();
@@ -135,7 +64,7 @@ export default function AuxiliaryBar() {
 
       {selectedTask ? (
         // A key prop is used to force a remount of the component
-        <MyForm key={selectedTask.uuid} task={selectedTask} />
+        <EditTaskForm key={selectedTask.uuid} task={selectedTask} />
       ) : (
         <div className="size-full px-3 pb-1.5">
           {isFetching ? (
