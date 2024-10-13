678944c515c909ece1f721f7833d890efd9ae14a
Bryton Lacquement <hello@brytonlacquement.com>
2024-10-13 12:36:00 +0200

    EditTaskForm: drop the save button

--- src/components/features/forms/edit-task-form.tsx
+++ src/components/features/forms/edit-task-form.tsx
@@ -38,92 +38,82 @@ function EditTaskForm({ task }: EditTaskFormProps) {
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="flex-container flex flex-1 flex-col gap-3">
-        <ScrollArea>
-          <GroupList>
-            <FormGroup name="Description">
-              <FormField
-                control={form.control}
-                name="description"
-                render={({ field }) => (
-                  <FormItemWrapper>
-                    <Input placeholder="Enter a description…" autoFocus {...field} />
-                  </FormItemWrapper>
-                )}
-              />
-            </FormGroup>
+        <GroupList>
+          <FormGroup name="Description">
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItemWrapper>
+                  <Input placeholder="Enter a description…" autoFocus {...field} />
+                </FormItemWrapper>
+              )}
+            />
+          </FormGroup>
 
-            <FormGroup name="Action">
-              <FormField
-                control={form.control}
-                name="status"
-                render={({ field }) => (
-                  <FormItemWrapper label="Status">
-                    <Input placeholder="Enter a description…" {...field} />
-                  </FormItemWrapper>
-                )}
-              />
-              <FormItemWrapper label="ID">
-                <PlainInput value={task.id} />
-              </FormItemWrapper>
-              <FormItemWrapper label="UUID">
-                <PlainInput value={task.uuid} />
-              </FormItemWrapper>
-            </FormGroup>
+          <FormGroup name="Action">
+            <FormField
+              control={form.control}
+              name="status"
+              render={({ field }) => (
+                <FormItemWrapper label="Status">
+                  <Input placeholder="Enter a description…" {...field} />
+                </FormItemWrapper>
+              )}
+            />
+            <FormItemWrapper label="ID">
+              <PlainInput value={task.id} />
+            </FormItemWrapper>
+            <FormItemWrapper label="UUID">
+              <PlainInput value={task.uuid} />
+            </FormItemWrapper>
+          </FormGroup>
 
-            <FormGroup name="Dates">
-              <FormField
-                control={form.control}
-                name="due"
-                render={({ field }) => (
-                  <FormItemWrapper label="Due">
-                    <DatePicker {...field} />
-                  </FormItemWrapper>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="scheduled"
-                render={({ field }) => (
-                  <FormItemWrapper label="Scheduled">
-                    <DatePicker {...field} />
-                  </FormItemWrapper>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="wait"
-                render={({ field }) => (
-                  <FormItemWrapper label="Wait">
-                    <DatePicker {...field} />
-                  </FormItemWrapper>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="until"
-                render={({ field }) => (
-                  <FormItemWrapper label="Until">
-                    <DatePicker {...field} />
-                  </FormItemWrapper>
-                )}
-              />
-              <FormItemWrapper label="Entry">
-                <PlainInput value={toLocaleDateString(task.entry)} />
-              </FormItemWrapper>
-              <FormItemWrapper label="Modified">
-                <PlainInput value={toLocaleDateString(task.modified)} />
-              </FormItemWrapper>
-            </FormGroup>
-          </GroupList>
-
-          <div className="h-24" />
-        </ScrollArea>
-
-        <ButtonList className="mx-3 mb-3" orientation="vertical">
-          <Button type="submit" disabled={!form.formState.isDirty}>
-            Save
-          </Button>
-        </ButtonList>
+          <FormGroup name="Dates">
+            <FormField
+              control={form.control}
+              name="due"
+              render={({ field }) => (
+                <FormItemWrapper label="Due">
+                  <DatePicker {...field} />
+                </FormItemWrapper>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="scheduled"
+              render={({ field }) => (
+                <FormItemWrapper label="Scheduled">
+                  <DatePicker {...field} />
+                </FormItemWrapper>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="wait"
+              render={({ field }) => (
+                <FormItemWrapper label="Wait">
+                  <DatePicker {...field} />
+                </FormItemWrapper>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="until"
+              render={({ field }) => (
+                <FormItemWrapper label="Until">
+                  <DatePicker {...field} />
+                </FormItemWrapper>
+              )}
+            />
+            <FormItemWrapper label="Entry">
+              <PlainInput value={toLocaleDateString(task.entry)} />
+            </FormItemWrapper>
+            <FormItemWrapper label="Modified">
+              <PlainInput value={toLocaleDateString(task.modified)} />
+            </FormItemWrapper>
+          </FormGroup>
+        </GroupList>
       </form>
     </Form>
   );
